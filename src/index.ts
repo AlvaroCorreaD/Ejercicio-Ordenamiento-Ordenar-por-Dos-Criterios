c726b6bce8f6ddde74b6833be33d9cde8af6c08b
AlvaroCorreaD <104107312+AlvaroCorreaD@users.noreply.github.com>
2022-07-10 18:43:48 -0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 let n = Number(prompt("Input Array Names dimention"));
 let arrayNames: string[] = new Array(n);
+let i: number;
 
 function setArrayAges(minAge: number = 18, maxAge: number = 66) {
   let dimention = Number(prompt("Input Array Ages dimention:"));
@@ -19,11 +20,27 @@ function setArrayHeights(minH: number = 165, maxH: number = 201) {
   return arrayHeights;
 }
 
-let arrayAges = setArrayAges();
-let arrayHeights = setArrayHeights();
+function bubbleSortValues(sortedArray: number[]) {
+  for (let i = 0; i < sortedArray.length; i++) {
+    for (let j = 0; j < sortedArray.length; j++) {
+      if (sortedArray[j] > sortedArray[j + 1]) {
+        let temp = sortedArray[j];
+        sortedArray[j] = sortedArray[j + 1];
+        sortedArray[j + 1] = temp;
+      }
+    }
+  }
+  return sortedArray;
+}
+
+let arrayAges: number[] = setArrayAges();
+let arrayHeights: number[] = setArrayHeights();
 
 for (let i = 0; i < arrayNames.length; i++) {
   arrayNames[i] = String(prompt("Input Names"));
+}
+
+for (i = 0; i < arrayAges.length; i++) {
   console.log(
     "Nombre:",
     arrayNames[i],
