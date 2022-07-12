57c885418232268a01a7a122a01a9b3a6d93ecb4
irina-marzioni-tdf <104698059+irina-marzioni-tdf@users.noreply.github.com>
2022-07-12 12:05:19 -0300

    initial commit

--- src/index.ts
+++ src/index.ts
@@ -1,52 +1,92 @@
-let n = Number(prompt("Input Array Names dimention"));
-let arrayNames: string[] = new Array(n);
-let i: number;
+function setArrayNames(dimention: number) {
+  let arrayNames: string[] = new Array(dimention);
+  for (let i = 0; i < dimention; i++) {
+    arrayNames[i] = String(prompt("Input Name " + i));
+  }
+  return arrayNames;
+}
 
-function setArrayAges(minAge: number = 18, maxAge: number = 66) {
-  let dimention = Number(prompt("Input Array Ages dimention:"));
+function setArrayAges(minAge: number, maxAge: number, dimention: number) {
   let arrayAges: number[] = new Array(dimention);
-  for (let i = 0; i < arrayAges.length; i++) {
+  for (let i = 0; i < dimention; i++) {
     arrayAges[i] = Math.floor(Math.random() * (maxAge - minAge) + minAge);
   }
   return arrayAges;
 }
 
-function setArrayHeights(minH: number = 165, maxH: number = 201) {
-  let dimention = Number(prompt("Input Array Heights dimention:"));
+function setArrayHeights(minH: number, maxH: number, dimention: number) {
   let arrayHeights: number[] = new Array(dimention);
-  for (let i = 0; i < arrayHeights.length; i++) {
+  for (let i = 0; i < dimention; i++) {
     arrayHeights[i] = Math.floor(Math.random() * (maxH - minH) + minH);
   }
   return arrayHeights;
 }
 
-function bubbleSortValues(sortedArray: number[]) {
-  for (let i = 0; i < sortedArray.length; i++) {
-    for (let j = 0; j < sortedArray.length; j++) {
-      if (sortedArray[j] > sortedArray[j + 1]) {
-        let temp = sortedArray[j];
-        sortedArray[j] = sortedArray[j + 1];
-        sortedArray[j + 1] = temp;
+function printArrays(
+  arrayNames: string[],
+  arrayAges: number[],
+  arrayHeights: number[],
+  dimention: number
+) {
+  for (let i = 0; i < dimention; i++) {
+    console.log(
+      "Nombre:",
+      arrayNames[i],
+      "Edad:",
+      arrayAges[i] + " años",
+      "Altura:",
+      arrayHeights[i] + "Cm"
+    );
+  }
+  return arrayHeights;
+}
+
+function comparator(arrayAges: number[], arrayHeights: number[], i: number) {
+  let comparacion: number = 0;
+  if (arrayAges[i] > arrayAges[i + 1]) {
+    comparacion = 1;
+  } else if (arrayAges[i] === arrayAges[i + 1]) {
+    if (arrayHeights[i] > arrayHeights[i + 1]) {
+      comparacion = 1;
+    } else if (arrayHeights[i] < arrayHeights[i + 1]) {
+      comparacion = -1;
+    }
+  } else {
+    comparacion = -1;
+  }
+  return comparacion;
+}
+
+function change(array: any[], i: number) {
+  let aux: string;
+  aux = array[i];
+  array[i] = array[i + 1];
+  array[i + 1] = aux;
+}
+
+function bubbleSortValues(
+  arrayNames: string[],
+  arrayAges: number[],
+  arrayHeights: number[],
+  dimention: number
+) {
+  for (let i = 0; i < dimention; i++) {
+    for (let j = 0; j < dimention; j++) {
+      if (comparator(arrayAges, arrayHeights, j) === 1) {
+        change(arrayNames, j);
+        change(arrayAges, j);
+        change(arrayHeights, j);
       }
     }
   }
-  return sortedArray;
 }
 
-let arrayAges: number[] = setArrayAges();
-let arrayHeights: number[] = setArrayHeights();
+let n = Number(prompt("Array dimention"));
 
-for (let i = 0; i < arrayNames.length; i++) {
-  arrayNames[i] = String(prompt("Input Names"));
-}
+let names: string[] = setArrayNames(n);
+let ages: number[] = setArrayAges(22, 40, n);
+let heights: number[] = setArrayHeights(165, 201, n);
 
-for (i = 0; i < arrayAges.length; i++) {
-  console.log(
-    "Nombre:",
-    arrayNames[i],
-    "Edad:",
-    arrayAges[i] + " años",
-    "Altura:",
-    arrayHeights[i] + "Cm"
-  );
-}
+printArrays(names, ages, heights, n);
+bubbleSortValues(names, ages, heights, n);
+printArrays(names, ages, heights, n);
